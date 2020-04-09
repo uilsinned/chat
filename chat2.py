62e3ca98e1e85bc91f9e3c46e83ba69ee7a93ffc
Dennis Liu <dennis1224z@gmail.com>
2020-04-09 14:18:14 +0800

    add string split

---
+++ chat2.py
@@ -0,0 +1,11 @@
+#練習字串分割
+lines = []
+with open('3.txt', 'r', encoding='utf-8-sig')as f:
+	for line in f:
+		lines.append(line.strip())
+
+for line in lines:
+	s = line.split(' ')
+	time = s[0][:5]
+	name = s[0][5:]
+	print([time, name])
