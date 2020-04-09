26fca1d093dfa495ba6dbbf7f2f9503df54b0ccb
Dennis Liu <dennis1224z@gmail.com>
2020-04-09 11:07:55 +0800

    add convert function

--- chat.py
+++ chat.py
@@ -4,14 +4,24 @@ def input_file(filename):
     chat = []
     with open(filename, 'r', encoding='utf-8-sig' ) as f:
         for line in f:
-            if 'Allen' in line:
-                name = 'Allen'
-                continue
-            elif 'Tom' in line:
-                name = 'Tom'
-                continue
-            chat.append(name + ': ' + line.strip())
+            chat.append(line.strip())        
     return chat
+
+def convert(chat):
+    new = []
+    name = None
+    for line in chat:
+        if 'Allen' in line:
+            name = 'Allen'
+            continue
+        elif 'Tom' in line:
+            name = 'Tom'
+            continue
+        if name:
+            new.append(name + ': ' + line.strip()) 
+    return new
+   
+
 
 def output_file(filename, chat):
     with open(filename, 'w', encoding='utf-8' ) as f:
@@ -20,6 +30,7 @@ def output_file(filename, chat):
             
 def main():
     chat = input_file('input.txt')
+    chat = convert(chat)
     output_file('output.txt', chat)
 
 main()
